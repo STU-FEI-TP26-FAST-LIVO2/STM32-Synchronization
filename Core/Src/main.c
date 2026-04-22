4d69426f4ebafc67f8c303362acbf062a62b5b3c
Michal Baculák <michal.baculak@gmail.com>
2026-04-22 15:52:32 +0200

    Switch UART2 communication to interrupt-based, improving 1 IMU payload duration

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -174,11 +174,6 @@ int main(void)
 	{
 		imu_drdy_flag = 0;
 		imu_dma_busy_flag = 1;
-		// c6dofimu24_read_data(&imu_data);
-//		HAL_StatusTypeDef imu_status = c6dofimu24_read_data_raw(imu_data_raw);
-//		HAL_StatusTypeDef imu_status = c6dofimu24_read_data(&imu_data);
-//		HAL_StatusTypeDef imu_status = c6dofimu24_read_data(&imu_data);
-
 	    HAL_StatusTypeDef status = HAL_I2C_Mem_Read_IT(
 	                                &hi2c1,
 	                                C6DOFIMU24_DEVICE_ADDRESS,
@@ -186,14 +181,6 @@ int main(void)
 	                                1,
 									imu_data_raw,
 	                                14);
-
-	    // sprintf(uart_debug_msg, "DRDY int received, init I2C DMA with status %d, UART status from IT is %d\r\n", status, uart_status_from_IT);
-		// uart_status = HAL_UART_Transmit(&huart2, (uint8_t*)uart_debug_msg, strlen(uart_debug_msg), 100);
-//		imu_data_raw[14] = '\0';
-//		c6dofimu24_clear_data_ready();
-//		sprintf(uart_debug_msg, "IMU data: accel_x: %d, temp: %d\r\n", (int)(imu_data.accel.x*100), (int)(imu_data.temperature*100));
-
-//		uart_status = HAL_UART_Transmit(&huart2, (uint8_t*)imu_data_raw, IMU_DATA_RAW_SIZE, 100);
 	}
 
     /* USER CODE END WHILE */
@@ -252,9 +239,6 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 	if(GPIO_Pin == IMU_DRDY_Pin)
 	{
 		imu_drdy_flag = 1;
-		// sprintf(uart_debug_msg, "EXTI DRDY interrupt\r\n");
-		// uart_status = HAL_UART_Transmit(&huart2, (uint8_t*)uart_debug_msg, strlen(uart_debug_msg), 100);
-
 	}
 }
 void HAL_I2C_ErrorCallback(I2C_HandleTypeDef *hi2c)
@@ -307,8 +291,7 @@ void HAL_I2C_AddrCallback(I2C_HandleTypeDef *hi2c, uint8_t TransferDirection, ui
 
 void HAL_I2C_MemRxCpltCallback(I2C_HandleTypeDef *hi2c)
 {
-
-	uart_status_from_IT = HAL_UART_Transmit(&huart2, (uint8_t*)imu_data_raw, IMU_DATA_RAW_SIZE, 100);
+	uart_status_from_IT = HAL_UART_Transmit_IT(&huart2, (uint8_t*)imu_data_raw, IMU_DATA_RAW_SIZE);
 	c6dofimu24_clear_data_ready();
 	imu_dma_busy_flag = 0;
 }
--- Core/Src/stm32f1xx_it.c
+++ Core/Src/stm32f1xx_it.c
@@ -60,6 +60,7 @@ extern DMA_HandleTypeDef hdma_i2c1_tx;
 extern I2C_HandleTypeDef hi2c1;
 extern DMA_HandleTypeDef hdma_usart1_tx;
 extern UART_HandleTypeDef huart1;
+extern UART_HandleTypeDef huart2;
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -299,6 +300,20 @@ void USART1_IRQHandler(void)
   /* USER CODE BEGIN USART1_IRQn 1 */
 
   /* USER CODE END USART1_IRQn 1 */
+}
+
+/**
+  * @brief This function handles USART2 global interrupt.
+  */
+void USART2_IRQHandler(void)
+{
+  /* USER CODE BEGIN USART2_IRQn 0 */
+
+  /* USER CODE END USART2_IRQn 0 */
+  HAL_UART_IRQHandler(&huart2);
+  /* USER CODE BEGIN USART2_IRQn 1 */
+
+  /* USER CODE END USART2_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
